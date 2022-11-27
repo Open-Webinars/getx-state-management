70a6dd3f6a1cd23a8045892d16ea13a3ccb0bb31
jose09511 <josemanuelbarbasanchez@gmail.com>
2022-11-27 19:43:33 +0100

    separate different types management

--- android/app/src/main/kotlin/com/example/calculator_flutter_getx/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.calculator_flutter_getx
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- lib/controllers/calculator_controller.dart
+++
@@ -1,9 +0,0 @@
-import 'package:get/get.dart';
-
-class CalculatorController extends GetxController {
-  // TextEditingController txtEntrada = TextEditingController();
-  // TextEditingController txtResultado = TextEditingController();
-
-  var txtEntrada = "0".obs;
-  var txtResultado = "Resultado".obs;
-}
